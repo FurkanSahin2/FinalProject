694b091d2ed0f557e54e253ab03a4a6e4b603199
M. Furkan Şahin <aliince.000000@gmail.com>
2023-01-13 13:37:29 +0300

    Add project files.

---
+++ Entities/Concrete/Category.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.Concrete
+{
+
+    // "Çıplak Class Kalmasın" Standartı 
+    public class Category
+    {
+        public int CategoryId { get; set; }
+        public string CategoryName { get; set; }
+    }
+}
