6166f9c830f9c5058ff90b555578901f32a6b933
M. Furkan Şahin <aliince.000000@gmail.com>
2023-01-13 14:25:34 +0300

    13 Aralık 2023 Ders Sonu

--- Entities/Concrete/Category.cs
+++ Entities/Concrete/Category.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Entities.Abstract;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,7 +7,7 @@ namespace Entities.Concrete
 {
 
     // "Çıplak Class Kalmasın" Standartı 
-    public class Category
+    public class Category:IEntity
     {
         public int CategoryId { get; set; }
         public string CategoryName { get; set; }
