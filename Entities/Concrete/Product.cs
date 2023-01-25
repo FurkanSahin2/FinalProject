ef832eac351fbbc9ebd371eb199f5658c17122d1
M. Furkan Şahin <aliince.000000@gmail.com>
2023-01-25 12:13:59 +0300

    24.01.2023 Ders Sonu

--- Entities/Concrete/Product.cs
+++ Entities/Concrete/Product.cs
@@ -7,10 +7,10 @@ namespace Entities.Concrete
 {
     public class Product : IEntity
     {
-        public int ProductId { get; set; }
-        public int CategoryId { get; set; }
+        public int ProductId { get; set; } // Primary Key
+        public int CategoryId { get; set; } // Foreign Key
         public string ProductName { get; set; }
-        public short UnitsInStock { get; set; }
+        public short UnitsInStock { get; set; } // short = Int16
         public decimal UnitPrice { get; set; }
     }
 }
