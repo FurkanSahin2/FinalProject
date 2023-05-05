8f41b623c00e75cfda54920f60e3ceb59b425adb
Furkan Sahin <aliince.000000@gmail.com>
2023-05-06 02:28:24 +0300

    .

--- WebAPI/Startup.cs
+++ WebAPI/Startup.cs
@@ -27,7 +27,7 @@ namespace WebAPI
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
+        public void ConfigureServices(IServiceCollection services) s
         {
             services.AddControllers();
             services.AddSingleton<IProductService, ProductManager>();
